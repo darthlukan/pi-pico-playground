b88d851ad2adbee0e281734032b40e0c5b69340a
Brian Tomlinson <brian.tomlinson@thatgamecompany.com>
2024-12-28 14:02:40 -0800

    We can blink the onboard LED of the Pico/RP2040 using PIO
    Added many comments because I'll forget why this works without them.

--- blink-pio/blink.c
+++ blink-pio/blink.c
@@ -5,35 +5,41 @@
 #include "hardware/clocks.h"
 #include "blink.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
 
 #ifndef PIO_BLINK_LED_GPIO
 #define PIO_BLINK_LED_GPIO PICO_DEFAULT_LED_PIN
 #endif
 
+
 int main() {
+    // On RP2040 this is `25`, RP2035 it's on the Wireless chip
     assert(PIO_BLINK_LED_GPIO < 31);
 
     PIO pio;
     uint sm;
     uint offset;
+    uint SLEEP_DELAY_MS = 1000;
+    float SM_CLK_FREQ = 2500; // hz
 
     setup_default_uart();
 
+    // Find an open PIO and state machine on that PIO, add our PIO Assembly Program to it, and specify the GPIO pins
+    // which should be mapped/available to the PIO and State Machine
     bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&blink_program, &pio, &sm, &offset, PIO_BLINK_LED_GPIO, 1, true);
+    // If the previous statement is not `true`, fail catastrophically.
     hard_assert(success);
 
+    // Some text to STDOUT so we can see something via the Serial Console
     printf("Using gpio %d\n", PIO_BLINK_LED_GPIO);
-    blink_program_init(pio, sm, offset, PIO_BLINK_LED_GPIO);
 
+    // Initialize the state machine and tell it to run our assembly program
+    blink_program_init(pio, sm, offset, PIO_BLINK_LED_GPIO, SM_CLK_FREQ);
+
+    // Infinite loop with no break-out condition: DO NOT DO THIS IN PROD!
     while (true) {
-        pio_sm_put_blocking(pio, sm, 1);
-        printf("GPIO %d is on\n", PIO_BLINK_LED_GPIO);
-        sleep_ms(1000);
-        pio_sm_put_blocking(pio, sm, 0);
-        printf("GPIO %d is off", PIO_BLINK_LED_GPIO);
-        sleep_ms(1000);
+        sleep_ms(SLEEP_DELAY_MS);
     }
 
+    // If we do somehow break out of the infinite loop, clean up our PIO
     pio_remove_program_and_unclaim_sm(&blink_program, pio, sm, offset);
 }
