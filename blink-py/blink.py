ec0defdefcf20e8ece3505e899b70ca688472717
Brian Tomlinson <brian.tomlinson@thatgamecompany.com>
2024-12-27 23:09:32 -0800

    update blink.py to toggle instead of being so pedantic. Add .gitignore

--- blink-py/blink.py
+++ blink-py/blink.py
@@ -1,15 +1,14 @@
 from machine import Pin
 from utime import sleep
 
-GP15 = 15
-GP25 = 25
+
+GP25 = 25 # AKA: "LED"
 DELAY = 1
 
-led_external = Pin(GP15, Pin.OUT)
+
 led_internal = Pin(GP25, Pin.OUT)
 
+
 while True:
-    led_external.toggle()
     sleep(DELAY)
     led_internal.toggle()
-    sleep(DELAY)
